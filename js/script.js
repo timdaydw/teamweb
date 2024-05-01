0ff0f9da700157f5117dc68b7e39bc64e4438051
timdaydw <timdaydw@gmail.com>
2024-05-01 21:25:15 +0800

    0501

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(function () {
+    
+});
+
+// 啟用smoove 共用區 (一定要單獨寫在最外面)
+$('.smoove').smoove({
+    offset: '30%',
+});
+
+$('.star').smoove({
+    offset: '90%',
+});
+
+$('.leaf-left-bg').smoove({
+    offset: '40%',
+});
+$('.leaf-right-bg').smoove({
+    offset: '10%',
+});
+$('.light-bg').smoove({
+    offset: '10%',
+});
+$('.product-title').smoove({
+    offset: '50%',
+});
